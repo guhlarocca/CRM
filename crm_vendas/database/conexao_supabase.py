506955db8e799f5ebf08ab58f213a106301dfdf5
Gustavo Larocca <guh.larocca@gmail.com>
2024-12-03 13:55:43 -0300

    fix: Corrige implementação do ConexaoSupabase e atualiza dependências

--- crm_vendas/database/conexao_supabase.py
+++ crm_vendas/database/conexao_supabase.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import logging
+from supabase import create_client, Client
 
 # Configurar logging
 logging.basicConfig(level=logging.INFO)
@@ -10,41 +11,12 @@ load_dotenv()
 
 class ConexaoSupabase:
     def __init__(self):
-        self.db_config = {
-            'dbname': os.getenv('DB_NAME', 'postgres'),
-            'user': os.getenv('DB_USER'),
-            'password': os.getenv('DB_PASS'),
-            'host': os.getenv('DB_HOST'),
-            'port': os.getenv('DB_PORT', '5432')
-        }
-        self._psycopg2 = None
+        self.supabase_url = os.getenv('SUPABASE_URL')
+        self.supabase_key = os.getenv('SUPABASE_KEY')
+        self.client = create_client(self.supabase_url, self.supabase_key)
 
-    @property
-    def psycopg2(self):
-        if self._psycopg2 is None:
-            try:
-                import psycopg2
-                self._psycopg2 = psycopg2
-            except ImportError:
-                logger.error("Erro ao importar psycopg2")
-                raise
-        return self._psycopg2
-
-    def get_connection(self):
-        try:
-            # Tentar importar e usar o psycopg2
-            pg = self.psycopg2
-            conn = pg.connect(**self.db_config)
-            # Testar a conexão
-            with conn.cursor() as cur:
-                cur.execute('SELECT 1')
-            return conn
-        except Exception as e:
-            logger.error(f"Erro na conexão: {str(e)}")
-            return None
-
-    def get_session(self):
-        return self.get_connection()
+    def table(self, nome_tabela):
+        return self.client.table(nome_tabela)
 
 def criar_cliente_supabase():
     try:
